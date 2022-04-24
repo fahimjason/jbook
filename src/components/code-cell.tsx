c092871a597f126e5f8db1195dd847eda2ef1930
JisanFahim <fahimjason@users.noreply.github.com>
2022-04-24 17:54:07 +0600

    refactored code

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import bundler from '../bundler';
+
+const CodeCell = () => {
+    const [code, setCode] = useState('');
+    const [input, setInput] = useState('');
+
+    const onClick = async () => {
+        const output = await bundler(input);
+
+        setCode(output);
+    };
+
+    return (
+        <div>
+            <CodeEditor
+                initialValue='const a = 1;'
+                onChange={(value) => setInput(value)}
+            />
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <Preview code={code} />
+        </div>
+    );
+}
+
+export default CodeCell;
