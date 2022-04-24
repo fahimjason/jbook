8a619bc27a138c2b912b14d943ff73b9fa9bcbe2
JisanFahim <fahimjason@users.noreply.github.com>
2022-04-25 02:48:38 +0600

    added resizable component

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import bundler from '../bundler';
+import Resizable from './resizable';
 
 const CodeCell = () => {
     const [code, setCode] = useState('');
@@ -15,16 +16,18 @@ const CodeCell = () => {
     };
 
     return (
-        <div>
-            <CodeEditor
-                initialValue='const a = 1;'
-                onChange={(value) => setInput(value)}
-            />
+        <Resizable direction="vertical">
             <div>
-                <button onClick={onClick}>Submit</button>
+                <CodeEditor
+                    initialValue='const a = 1;'
+                    onChange={(value) => setInput(value)}
+                />
+                <div>
+                    <button onClick={onClick}>Submit</button>
+                </div>
+                <Preview code={code} />
             </div>
-            <Preview code={code} />
-        </div>
+        </Resizable>
     );
 }
 
