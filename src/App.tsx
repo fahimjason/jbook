1f0762361cba78b660351427767b8c6f8bd3a6b2
JisanFahim <fahimjason@users.noreply.github.com>
2022-04-26 00:14:42 +0600

    fixed event bubbling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import CodeCell from "./components/code-cell";
+// import CodeCell from "./components/code-cell";
+import TextEditor from "./components/text-editor";
 
 const App = () => {
 
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
+      {/* <CodeCell /> */}
     </div>
   );
 }
---
+++ src/components/text-editor.tsx
@@ -0,0 +1,43 @@
+import './text-editor.css';
+import { useState, useEffect, useRef } from 'react';
+import MDEditor from "@uiw/react-md-editor";
+
+const TextEditor = () => {
+    const ref = useRef<HTMLDivElement | null>(null);
+    const [editing, setEditing] = useState(false);
+
+    useEffect(() => {
+        const listener = (event: MouseEvent) => {
+            if (
+                ref.current &&
+                event.target &&
+                ref.current.contains(event.target as Node)
+            ) {
+                return;
+            }
+
+            setEditing(false);
+        };
+        document.addEventListener('click', listener, { capture: true });
+
+        return () => {
+            document.removeEventListener('click', listener, { capture: true });
+        };
+    }, []);
+
+    if (editing) {
+        return (
+            <div ref={ref}>
+                <MDEditor />
+            </div>
+        );
+    }
+
+    return (
+        <div onClick={() => setEditing(true)}>
+            <MDEditor.Markdown source={'# Header'} />
+        </div>
+    );
+}
+
+export default TextEditor;
