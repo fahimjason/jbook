9341c654fc895561c746e24e8b827fa4420342e4
JisanFahim <fahimjason@users.noreply.github.com>
2022-04-25 12:15:13 +0600

    done horizontal resizing

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -18,10 +18,12 @@ const CodeCell = () => {
     return (
         <Resizable direction="vertical">
             <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
-                <CodeEditor
-                    initialValue='const a = 1;'
-                    onChange={(value) => setInput(value)}
-                />
+                <Resizable direction="horizontal">
+                    <CodeEditor
+                        initialValue='const a = 1;'
+                        onChange={(value) => setInput(value)}
+                    />
+                </Resizable>
                 {/* <div>
                     <button onClick={onClick}>Submit</button>
                 </div> */}
