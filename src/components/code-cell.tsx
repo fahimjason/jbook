b543358078d4864630aaef2651b06d16cdb94b7b
JisanFahim <fahimjason@users.noreply.github.com>
2022-04-29 02:47:07 +0600

    added loader

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,5 +1,5 @@
+import './code-cell.css';
 import { useEffect } from 'react';
-
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import Resizable from './resizable';
@@ -18,6 +18,11 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
     useEffect(() => {
+        if (!bundle) {
+            createBundle(cell.id, cell.content);
+            return;
+        }
+
         const timer = setTimeout(async () => {
             createBundle(cell.id, cell.content);
         }, 750);
@@ -25,6 +30,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
         return () => {
             clearTimeout(timer);
         };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [cell.content, cell.id, createBundle]);
 
     return (
@@ -36,7 +42,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
                         onChange={(value) => updateCell(cell.id, value)}
                     />
                 </Resizable>
-                {bundle && <Preview code={bundle.code} err={bundle.err} />}
+                <div className="progress-wrapper">
+                    {!bundle || bundle.loading ? (
+                        <div className="progress-cover">
+                            <progress className="progress is-small is-primary" max="100">
+                                Loading
+                            </progress>
+                        </div>
+                    ) : (
+                        <Preview code={bundle.code} err={bundle.err} />
+                    )}
+                </div>
             </div>
         </Resizable>
     );
