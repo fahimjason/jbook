ee510ec32c8689f56399991f8bd071c9f4dcafce
JisanFahim <fahimjason@users.noreply.github.com>
2022-04-29 23:47:48 +0600

    added cumulative execution

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -16,22 +16,57 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
     // @ts-ignore
     const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+    const cumulativeCode = useTypedSelector((state) => {
+        // @ts-ignore
+        const { data, order } = state.cells;
+        // @ts-ignore
+        const orderedCells = order.map(id => data[id]);
+
+        const cumulativeCode = [
+            `
+            const show = (value) => {
+                const root = document.querySelector('#root');
+
+                if(typeof value === 'object') {
+                   if(value.$$typeof && value.props) {
+                       ReactDOM.render(value, root);
+                   } else {
+                    root.innerHTML = JSON.stringify(value);
+                   }
+                } else {
+                    root.innerHTML = value;
+                }
+            };
+            `
+        ];
+        for (let c of orderedCells) {
+            if (c.type === 'code') {
+                cumulativeCode.push(c.content);
+            }
+
+            if (c.id === cell.id) {
+                break;
+            }
+        }
+
+        return cumulativeCode;
+    });
 
     useEffect(() => {
         if (!bundle) {
-            createBundle(cell.id, cell.content);
+            createBundle(cell.id, cumulativeCode.join('\n'));
             return;
         }
 
         const timer = setTimeout(async () => {
-            createBundle(cell.id, cell.content);
+            createBundle(cell.id, cumulativeCode.join('\n'));
         }, 750);
 
         return () => {
             clearTimeout(timer);
         };
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [cell.content, cell.id, createBundle]);
+    }, [cumulativeCode.join('\n'), cell.id, createBundle]);
 
     return (
         <Resizable direction="vertical">
