8b97ae95c0d34963f5559c3b26797bccf684728e
JisanFahim <fahimjason@users.noreply.github.com>
2022-05-02 13:03:47 +0600

    created cell action creator

--- packages/local-client/src/state/actions/index.ts
+++ packages/local-client/src/state/actions/index.ts
@@ -1,5 +1,5 @@
-import { CellTypes } from '../cell';
 import { ActionType } from '../action-types';
+import { Cell, CellTypes } from '../cell';
 
 export type Direction = 'up' | 'down';
 export interface MoveCellAction {
@@ -49,10 +49,27 @@ export interface BundleCompleteAction {
     }
 }
 
+export interface FetchCellsAction {
+    type: ActionType.FETCH_CELLS,
+}
+
+export interface FetchCellsCompleteAction {
+    type: ActionType.FETCH_CELLS_COMPLETE,
+    payload: Cell[],
+}
+
+export interface FetchCellsErrorAction {
+    type: ActionType.FETCH_CELLS_ERROR,
+    payload: string,
+}
+
 export type Action =
     MoveCellAction
     | DeleteCellAction
     | InsertCellAfterAction
     | UpdateCellAction
     | BundleStartAction
-    | BundleCompleteAction;
+    | BundleCompleteAction
+    | FetchCellsAction
+    | FetchCellsCompleteAction
+    | FetchCellsErrorAction;
